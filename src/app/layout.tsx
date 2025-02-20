b746243b5d223506086e9008b9bde70b71ff25d8
dipyamanroy <d1pyamanroy@protonmail.com>
2025-02-20 22:05:36 +0530

    Final og/twitter image metadata fix.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
         <meta property="og:image" content="https://i.ibb.co/0RsFPwg0/Screenshot-from-2025-02-20-21-44-38.png" />
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
+        <meta property="og:image:type" content="png" />
         <meta property="og:url" content="https://5b-lander.vercel.app" />
         <meta property="og:type" content="website" />
 
@@ -30,7 +31,9 @@ export default function RootLayout({
         <meta name="twitter:title" content="5th Bridge - Specialists in Digital Transformation" />
         <meta name="twitter:description" content="We help businesses navigate the digital landscape with expertise and innovation." />
         <meta name="twitter:image" content="https://i.ibb.co/0RsFPwg0/Screenshot-from-2025-02-20-21-44-38.png" />
-
+        <meta property="twitter:image:width" content="1200" />
+        <meta property="twitter:image:height" content="630" />
+        <meta property="twitter:image:type" content="png" />
         <link
           rel="stylesheet"
           href="https://api.fontshare.com/css?f%5B%5D=switzer@400,500,600,700&amp;display=swap"
