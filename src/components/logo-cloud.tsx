e8dbc8dbdd1d497e8f35978b5636ae90d7a90d41
Dipyaman Roy <droy@fifthbridgetech.com>
2025-02-24 14:19:41 +0530

    Made logo cloud scrollable.

--- src/components/logo-cloud.tsx
+++ src/components/logo-cloud.tsx
@@ -1,40 +1,50 @@
-import { clsx } from 'clsx'
+"use client"
+import { motion } from 'framer-motion';
+import { clsx } from 'clsx';
 
 export function LogoCloud({
   className,
 }: React.ComponentPropsWithoutRef<'div'>) {
+  const logos = [
+    { alt: "SavvyCal", src: "/logo-cloud/1.svg" },
+    { alt: "Laravel", src: "/logo-cloud/2.svg" },
+    { alt: "Tuple", src: "/logo-cloud/3.svg" },
+    { alt: "Transistor", src: "/logo-cloud/4.svg" },
+    { alt: "Statamic", src: "/logo-cloud/5.svg" }
+  ];
+
   return (
-    <div
-      className={clsx(
-        className,
-        'flex justify-between max-sm:mx-auto max-sm:max-w-md max-sm:flex-wrap max-sm:justify-evenly max-sm:gap-x-4 max-sm:gap-y-4',
-      )}
-    >
-      <img
-        alt="SavvyCal"
-        src="/logo-cloud/1.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Laravel"
-        src="/logo-cloud/2.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Tuple"
-        src="/logo-cloud/3.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Transistor"
-        src="/logo-cloud/4.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Statamic"
-        src="/logo-cloud/5.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
+    <div className={clsx('relative w-full', className)}>
+      <motion.div 
+        className="flex gap-40 py-4"
+        animate={{
+          x: [0, -1035],
+        }}
+        transition={{
+          duration: 20,
+          ease: "linear",
+          repeat: Infinity,
+        }}
+      >
+        {/* First set of logos */}
+        {logos.map((logo, idx) => (
+          <img
+            key={`logo-1-${idx}`}
+            alt={logo.alt}
+            src={logo.src}
+            className="h-9 shrink-0 sm:h-8 lg:h-12"
+          />
+        ))}
+        {/* Duplicated set for seamless loop */}
+        {logos.map((logo, idx) => (
+          <img
+            key={`logo-2-${idx}`}
+            alt={logo.alt}
+            src={logo.src}
+            className="h-9 shrink-0 sm:h-8 lg:h-12"
+          />
+        ))}
+      </motion.div>
     </div>
-  )
+  );
 }
