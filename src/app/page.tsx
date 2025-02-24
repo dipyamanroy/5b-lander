2fb178c3d904015bcebe18ea7ddd308d0d78f367
Dipyaman Roy <droy@fifthbridgetech.com>
2025-02-24 10:41:49 +0530

    Fixed banner image rendering.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ function Hero() {
         alt="Banner Image"
         layout="fill" 
         objectFit="cover" 
-        className="absolute inset-2 rounded-[2.5rem] p-2"
+        className="absolute inset-2 rounded-[2.5rem] p-3"
         style={{ filter: "blur(1.5px)" }}
       />
       <Gradient className="absolute inset-2 p-3 rounded-4xl ring-1 ring-black/5 ring-inset" />
